7d740a3eea5c43d729ad5eb0ed8885127261d9a3
TheCharlatan <seb.kung@gmail.com>
2022-03-14 12:07:30 +0100

    Change command line arguments structure

--- main.py
+++ main.py
@@ -51,16 +51,6 @@ if __name__ == "__main__":
         description="Tool for parsing and analysing various blockchain data",
     )
     parser.add_argument(
-        "-i",
-        "--data-dir",
-        default="/home/drgrid/.bitcoin",
-        help = """Path the coin data directory, e.g. 
-            /home/drgrid.ethereum | 
-            home/drgrid/.bitmonero | 
-            /home/drgrid/.bitcoin
-            """
-    )
-    parser.add_argument(
         "-d",
         "--database",
         default="test.db",
@@ -70,36 +60,49 @@ if __name__ == "__main__":
         "-b",
         "--blockchain",
         default="bitcoin_mainnet",
-        help="""Coin to target. Only single targets are allowed, valid targets are: \n
-            bitcoin_mainnet   |
-            bitcoin_testnet3   |
-            bitcoin_regtest   |
-            monero_mainnet   |
-            monero_stagnet   |
-            monero_testnet   |
-            ethereum_mainnet
+        coices=("bitcoin_mainnet", "bitcoin_testnet3", "bitcoin_regtest", "monero_mainnet", "monero_stagenet", "monero_testnet", "ethereum_mainnet"),
+        help="Coin to target. Required argument, only single targets are allowed."
+    )
+    parser.add_argument(
+        "-p",
+        "--parse",
+        default="/home/drgrid/.bitcoin",
+        help="""Run the tool in parse mode to collect blockchain data, requires the blockchain data directory as argument, e.g
+                /home/drgrid/.ethereum
+                /home/drgrid/.bitmonero
+                /home/drgrid/.bitcoin
             """
     )
     parser.add_argument(
-        "-m",
-        "--mode",
-        default="parse",
-        help="""Mode of operation, either parses or analyzes data. Valid arguments are: \n
-            parse   |
-            analyze   |
-            view
-            """
+        "-a",
+        "--analyze",
+        help="Run the tool in analysis mode to detect specific data types",
+        choices=("ascii", "files")
+    )
+    parser.add_argument(
+        "-v",
+        "--view",
+        action="store_true",
+        help="Run the to tool in view mode to further analysis"
+    )
+    parser.add_argument(
+        "-c",
+        "--criteria",
+        default="ascii",
+        help="""analysis criteria to run against. Valid arguments are: \n
+            ascii |
+            files"""
     )
 
     # Parse the command line arguments
     args = parser.parse_args()
     path = args.data_dir
 
-    if args.mode == "parse":
-        parse(args.blockchain, args.data_dir, args.database)
-    elif args.mode == "analyze":
+    if args.parse is not None or args.parse is True:
+        parse(args.blockchain, args.parse, args.database)
+    if args.analyze == "acsii" or args.analyze == "files":
         analyze(args.blockchain, args.database)
-    elif args.mode == "view":
+    elif args.view:
         raise BaseException("view mode not implemented yet")
     else:
         raise BaseException("require a mode to run in")
