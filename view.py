ec7eeaeb3b09f5710c17a13175a79a89aa705947
TheCharlatan <seb.kung@gmail.com>
2022-03-25 14:29:41 +0100

    View: add more view-based magic filters

--- view.py
+++ view.py
@@ -48,6 +48,8 @@ class View:
         x2_pos = np.arange(34)
         color = self.get_matplotlib_color_from_blockchain()
 
+        print(accumulated_string_count, minimum_string_lengths)
+
         # truncate the histogram at 34 entries
         if len(lengths > 34):
             lengths = lengths[:34]
@@ -91,6 +93,7 @@ class View:
         png_count = 0
         gringotts_count = 0
         dif_count = 0
+        tar_archive_count = 0
         for file_type, count in zip(file_types, counts):
             if count == 1:
                 continue
@@ -109,7 +112,9 @@ class View:
             if "DIF" in file_type:
                 dif_count += 1
                 continue
-            if "object file" in file_type or "b.out" in file_type or "RISC OS" in file_type or "MMDF" in file_type or "Lotus" in file_type or "FuseCompress" in file_type or "FIGlet" in file_type or "AppleDouble" in file_type or "AppleSingle" in file_type or "MED_Song" in file_type or "Android binary" in file_type or "GDSII" in file_type or "SunOS" in file_type or "AppledDouble" in file_type or "Core file" in file_type or "MAthematica" in file_type or "Berkeley DB" in file_type or "Microstation" in file_type or "overlay object file" in file_type or "LADS" in file_type or "Netscape" in file_type or "ESRI Shapefile" in file_type or "Cytovision" in file_type or "i960" in file_type or "ddis" in file_type or "SPEC" in file_type or "MMFD" in file_type or "AHX" in file_type or "libfprint" in file_type or "SeqBox" in file_type or "Psion" in file_type or "PCP compiled" in file_type or "separate object" in file_type or "Compiled XKB" in file_type or "dar archive" in file_type or "cisco" in file_type or "Symbian" in file_type or "Spectrum .TAP" in file_type or "StuffIt" in file_type or "Spectrum" in file_type or "Spectrum" in file_type or "RAD" in file_type or "Psion Series" in file_type or "Progressive Graphics" in file_type or "Palm" in file_type or "LFS" in file_type or "GEM" in file_type or "ESRI Shapefile" in file_type or "keymap" in file_type or "Aster*x" in file_type:
+            if "tar archive" in file_type:
+                tar_archive_count += 1
+            if "core file (Xenix)" in file_type or "compiled Lisp" in file_type or "Zebra Metafile" in file_type or "StarOffice Gallery" in file_type or "Minix filesystem" in file_type or "Macintosh HFS" in file_type or "MacBinary" in file_type or "Embedded OpenType" in file_type or "DIY-Thermocam" in file_type or "Apple HFS" in file_type or "object file" in file_type or "b.out" in file_type or "RISC OS" in file_type or "MMDF" in file_type or "Lotus" in file_type or "FuseCompress" in file_type or "FIGlet" in file_type or "AppleDouble" in file_type or "AppleSingle" in file_type or "MED_Song" in file_type or "Android binary" in file_type or "GDSII" in file_type or "SunOS" in file_type or "AppledDouble" in file_type or "Core file" in file_type or "MAthematica" in file_type or "Berkeley DB" in file_type or "Microstation" in file_type or "overlay object file" in file_type or "LADS" in file_type or "Netscape" in file_type or "ESRI Shapefile" in file_type or "Cytovision" in file_type or "i960" in file_type or "ddis" in file_type or "SPEC" in file_type or "MMFD" in file_type or "AHX" in file_type or "libfprint" in file_type or "SeqBox" in file_type or "Psion" in file_type or "PCP compiled" in file_type or "separate object" in file_type or "Compiled XKB" in file_type or "dar archive" in file_type or "cisco" in file_type or "Symbian" in file_type or "Spectrum .TAP" in file_type or "StuffIt" in file_type or "Spectrum" in file_type or "Spectrum" in file_type or "RAD" in file_type or "Psion Series" in file_type or "Progressive Graphics" in file_type or "Palm" in file_type or "LFS" in file_type or "GEM" in file_type or "ESRI Shapefile" in file_type or "keymap" in file_type or "Aster*x" in file_type:
                 continue
             filtered_file_types.append(file_type)
             filtered_counts.append(count)
@@ -128,6 +133,9 @@ class View:
         if dif_count > 0:
             filtered_file_types.append("DIF (DVCPRO) movie file")
             filtered_counts.append(dif_count)
+        if tar_archive_count > 0:
+            filtered_file_types.append("tar archive")
+            filtered_counts.append(tar_archive_count)
         file_types = np.array(filtered_file_types)
         counts = np.array(filtered_counts)
 
